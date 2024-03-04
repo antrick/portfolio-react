d5949a99b19d8891009cff3f9ce5b1f1b59ff872
andy moreno <antrick10@gmail.com>
2024-03-03 23:55:09 -0600

    se elimina codigo comentado

--- src/Components.js/MainContent.js
+++ src/Components.js/MainContent.js
@@ -11,46 +11,7 @@ import ScrollToTopButton from './ScrollToTopButton'
 import skillsArray from '../localData/skillsArray'
 import { cmrSkills, hmsSkills } from '../localData/experienceData'
 
-const MainContent = () => {
-
-  // const cmrSkills = [
-  //   {
-  //     text: `Desarrollo de sitios web en WordPress para varios municipios,
-  //     asegurando la entrega de soluciones web efectivas y personalizadas para las
-  //     necesidades específicas de cada cliente.`,
-  //   },
-  //   {
-  //     text: `Participación en el desarrollo del sitio web de la empresa CMR
-  //     Consultores, utilizando HTML, CSS, Javascript y PHP para crear una plataforma web
-  //     funcional que reflejara la identidad y los servicios de la empresa.`,
-  //   },
-  //   {
-  //     text: `Participación en el desarrollo del sitio web en HTML, CSS, Javascript y PHP para la
-  //     empresa Ingeninn 360.`,
-  //   },
-   
-  //   {
-  //     text: `Desarrollo de modulos y catalogos en la versión beta de un sistema interno en PHP y Laravel,
-  //     diseñado para gestionar y optimizar los procesos de los servicios ofrecidos por la
-  //     empresa.`,
-  //   },
-  // ];
-  
-  // const hmsSkills = [
-  //   {
-  //     text: `Desarrollo y mantenimiento de módulos esenciales en dos sistemas ERP personalizados para una empresa multinacional, 
-  //     Utilizando  tecnologías como: PHP, Laravel, JavaScript, jQuery, GIT y SQL Server.`,
-  //   },
-  //   {
-  //     text: `Contribuí en actualizaciones y soporte de una aplicación móvil utilizando React Native y Typescript.`,
-  //   },
-  //   {
-  //     text: `Proporcioné soporte técnico integral a los usuarios del sistema en producción, gestionando correcciones y actualizaciones para garantizar un funcionamiento sin problemas.`,
-  //   },
-   
-  // ];
-
-  
+const MainContent = () => {  
   
   return (
     <main className='flex flex-col items-center p-4 '>
